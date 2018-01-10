41487fda861970191dd9b8de95ff0c343da2c687
schoeggel <schoeggel@gmx.ch>
2018-01-10 20:39:50 +0100

    db check

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
-	stats = require('./actions'),
-	db = require('./dbconn');	
+	stats = require('./actions');
+
+const conndetails = {
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+};
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection(conndetails);
+
+	
 console.log("Hello world!");
 console.log("Test stats 1. barcode 123 --> " + stats.artikelNr('123'));
 console.log("Test stats 2. artikel = 002056 --> " + JSON.stringify(stats.stats('002056')));
-
-	
 	
 http.createServer(function (req, res) {
   if (req.url.indexOf('/img') != -1) {
@@ -20,12 +29,16 @@ http.createServer(function (req, res) {
     });
 
 	} else if (req.url.indexOf('/dbcheck') != -1) {
-    res.writeHead(200, {'Content-Type': 'text/text'});
-      res.write('db check... --> ');
-	  console.log('db.check() --> ' + db.check());
-	  console.log('================ jetzt versucht er res.write... =============');
-	  res.write(db.check());
-      res.end();
+		res.writeHead(200, {'Content-Type': 'text/text'});
+		res.write('dbcheck... --> ');
+		console.log('dbcheck --> ');
+		connection.query('STATUS;', function (error, results, fields) {
+			if (error) throw error;
+			console.log('The solution is: ', results[0]);
+			res.write(results);
+			});
+		connection.end();
+		res.end();
 
   } else if (req.url.indexOf('/env') != -1) {
     var filePath = req.url.split('/db')[1];
--- dbconn.js
+++ dbconn.js
@@ -4,9 +4,41 @@ const conndetails = {
   password : process.env.RDS_PASSWORD,
   port     : process.env.RDS_PORT
 };
-
-
 exports.conndetails = conndetails;
+
+
+module.exports.execute = function(sql, onSucces, onError) {
+    var mysql = require('mysql');
+	var db = mysql.createConnection(conndetails);	
+	db.connect(function(err) {
+		if (err) {
+			console.log('dbconn ---> Database connection failed: ' + err.stack);
+	}});
+			
+    db.all(sql, function(err, rows) {
+        if (err) {
+            if (onError) {
+                onError(err);
+            } else {
+                console.log("Error: ", err);
+            }
+        } else {
+            if (onSucces) {
+                onSucces(rows);
+            } else {
+                console.log("Loaded data: ", rows);
+            }
+        }
+    });
+    db.close();
+};
+
+
+
+
+
+
+
 
 exports.check = function() {	
 	var mysql = require('mysql');
@@ -24,6 +56,6 @@ exports.check = function() {
 	connection.end();
 	console.log('db-connection test end.');
 	return (cnstatus);
-	}
+	};
 
 
